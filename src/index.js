4c85c0bae35ada5c8bd14b20f659ec5b6661c50f
Sathesh <sathesh.n@ramco.com>
2021-06-13 00:33:12 +0530

    added custom payload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyparser = require("body-parser");
 const dfff = require("dialogflow-fulfillment");
+const { platform } = require("os");
 
 const app = express();
 
@@ -14,9 +15,42 @@ app.post("/", express.json(), (request, response) => {
   function sendingResponse(agent) {
     agent.add("sending response from webhook server");
   }
+  function customPayload(agent){
+    const customPayload = {
+        "richContent": [
+          [
+            {
+              "type": "list",
+              "title": "List item 1 title",
+              "subtitle": "List item 1 subtitle",
+              "event": {
+                "name": "",
+                "languageCode": "",
+                "parameters": {}
+              }
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "type": "list",
+              "title": "List item 2 title",
+              "subtitle": "List item 2 subtitle",
+              "event": {
+                "name": "",
+                "languageCode": "",
+                "parameters": {}
+              }
+            }
+          ]
+        ]
+      }
+      agent.add( new dfff.Payload(platform.UNSPECIFIED, customPayload, {sendAsMessage : true, rawPayload : true}))
+    }
+
   const intentMap = new Map();
 
-  intentMap.set("issues", sendingResponse);
+  intentMap.set("issues", customPayload);
 
   agent.handleRequest(intentMap);
 });
