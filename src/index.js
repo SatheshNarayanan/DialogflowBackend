4dc3f4d25e672654a41803631892d3e8014f8bf6
Sathesh <sathesh.n@ramco.com>
2021-06-12 13:26:50 +0530

    first push

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const dfff = require("dialogflow-fulfillment");
+
+const app = express();
+
+app.post("/", express.json(), (request, response) => {
+  const agent = dfff.WebhookClient({
+    request,
+    response
+  });
+
+  function sendingResponse(agent) {
+    agent.add("sending response from webhook server");
+  }
+  const intentMap = new Map();
+
+  intentMap.set("Issue_type", sendingResponse);
+
+  agent.handleRequest(intentMap);
+});
+
+app.get("/", (request, response) => {
+  response.send("Hello");
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server is up and running!!");
+});
